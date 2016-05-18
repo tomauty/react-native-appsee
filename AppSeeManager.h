639ff1ba98ecb4109c51d7c2bf877faffff11bc1
Tom Auty <tom.auty@gmail.com>
2016-05-18 15:42:28 -0400

    Put some files in here

---
+++ AppSeeManager.h
@@ -0,0 +1,11 @@
+//
+//  AppSeeManager.h
+//
+//  Created by 坏小子不错的嘴唇 on 5/18/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+#import "RCTBridgeModule.h"
+
+@interface AppSeeManager : NSObject <RCTBridgeModule>
+@end
+
---
+++ AppSeeManager.m
@@ -0,0 +1,23 @@
+//
+//  AppSeeManager.m
+//
+//  Created by 坏小子不错的嘴唇 on 5/18/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+#import "AppSeeManager.h"
+#import <Appsee/Appsee.h>
+
+@implementation AppSeeManager
+
+RCT_EXPORT_MODULE(AppSee);
+RCT_EXPORT_METHOD(startScreen:(NSString *)screenName)
+{
+  [Appsee startScreen:screenName];
+};
+
+RCT_EXPORT_METHOD(addEvent:(NSString *)eventName)
+{
+  [Appsee addEvent:eventName];
+}
+
+@end
